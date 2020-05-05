5f9604302014ceb4d63aa6b3492a2e678a60a8a6
Ash Caplan <accaplan93@gmail.com>
2020-05-04 21:25:00 -0400

    blocking

--- web/client.js
+++ web/client.js
@@ -1,6 +1,6 @@
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-  projectId: '0k5ev4j1',
+  projectId: '8nnneie9',
   dataset: 'production',
   token: '', // or leave blank to be anonymous user
   useCdn: false, // `false` if you want to ensure fresh data
